b7dfeac0be9b2948f9bbfc7a6f4761a26e7926c0
Avea <aveakrause@gmail.com>
2019-10-26 12:44:53 -0700

    Update renderer

---
+++ Assets/Scripts/DancingGirl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DancingGirl : MonoBehaviour
+{
+    public GameObject player;
+    private void Update()
+    {
+        // Get the distance to the player
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+        // Move
+    }
+}
