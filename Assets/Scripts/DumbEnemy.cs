8f4353bdbfd3f50dfc7a0e7e40cfb6cf5a024dd8
Alfredo Cuevas <cuevasalfredo0@gmail.com>
2019-10-26 15:41:43 -0700

    DumbEnemy now spawns by fading in. Once completely opaque it begins to follow the player

--- Assets/Scripts/DumbEnemy.cs
+++ Assets/Scripts/DumbEnemy.cs
@@ -8,12 +8,13 @@ public class DumbEnemy : MonoBehaviour
     [Range(1f, 100f)]
     public float health = 10;
 
-    [Range(0f, 50f)]
-    [SerializeField] private float _speed = 5f;
+    [Range(0f, 50f)] [SerializeField] private float _speed = 5f;
+    [Range(.1f, 10)] [SerializeField] private float _spawnTimerInSeconds = 2;
     [SerializeField] private GameObject _target;
     
 
     private Rigidbody2D _rb;
+    private SpriteRenderer _spriteRenderer;
 
     private Dictionary<string, Action> States;
     private string _currentState;
@@ -23,15 +24,17 @@ public class DumbEnemy : MonoBehaviour
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
 
         States = new Dictionary<string, Action>();
-        _currentState = "DefaultState";
         States["DefaultState"] = DefaultState;
+        States["Spawn"] = Spawn;
     }
 
     private void Start()
     {
         moveVec = Vector2.zero;
+        EnterSpawnState();
     }
 
     void Update()
@@ -42,7 +45,7 @@ public class DumbEnemy : MonoBehaviour
     void FixedUpdate()
     {
         if (moveVec != Vector2.zero)
-            GetComponent<Rigidbody2D>().MovePosition((Vector2)(transform.position) + moveVec * _speed * Time.fixedDeltaTime);
+            _rb.MovePosition((Vector2)(transform.position) + moveVec * _speed * Time.fixedDeltaTime);
     }
 
     //STATES
@@ -54,6 +57,7 @@ public class DumbEnemy : MonoBehaviour
         DefaultState();
     }
 
+    // Default state is to chase the player
     void DefaultState()
     {
         SetMoveDir();
@@ -62,6 +66,22 @@ public class DumbEnemy : MonoBehaviour
     void ExitDefaultState()
     {
         
+    }
+
+    private void EnterSpawnState()
+    {
+        // Make Invisible
+        var color = _spriteRenderer.color;
+        color.a = 0;
+        _spriteRenderer.color = color;
+
+        _currentState = "Spawn";
+        Spawn();
+    }
+
+    private void ExitSpawnState()
+    {
+        EnterDefaultState();
     }
 
     //FUNCTIONS
@@ -74,5 +94,21 @@ public class DumbEnemy : MonoBehaviour
     void SetMoveDir()
     {
         moveVec = (Vector2)(_target.transform.position - transform.position).normalized;
+    }
+
+    private float _spawnCounter = 0;
+    private void Spawn()
+    {
+        _spawnCounter += Time.deltaTime/_spawnTimerInSeconds;
+        var color = _spriteRenderer.color;
+
+        color.a = Mathf.Lerp(0, 1, _spawnCounter);
+
+        _spriteRenderer.color = color;
+
+        if(_spriteRenderer.color.a >= 1f)
+        {
+            EnterDefaultState();
+        }
     }
 }
