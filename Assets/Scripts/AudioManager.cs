45b64c16b68c84e9ea3f1dc3b3e8498d7a2f8e1f
Daniel Allen Gutierrez <icanmakespaghetti@gmail.com>
2019-10-26 19:44:31 -0700

    Add Audio Manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+public class AudioManager : MonoBehaviour {
+    public Sound[] sounds;
+    public static AudioManager instance;
+    public GameObject player;
+
+	// Use this for initialization
+
+    void Update()
+    {
+        if(player != null)
+            transform.position = player.transform.position;
+    }
+
+	void Awake () {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+
+        DontDestroyOnLoad(gameObject);
+		foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+	}
+
+    void Start()
+    {
+
+
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+
+        }
+             
+        s.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+
+        }
+             
+        s.source.Stop();
+
+    }
+
+    public bool IsPlaying(string name)
+    {
+        Sound s = Array.Find(sounds,sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return false;
+        }
+        return s.source.isPlaying;
+    }
+}
