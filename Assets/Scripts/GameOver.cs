59a54791ea5651bd0e226e51663e045a1a7a3ef5
Nettsuka <jonathanchhou@outlook.com>
2019-10-26 17:35:45 -0700

    Added end scene

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameOver : MonoBehaviour
+{
+    public TextMeshProUGUI EndWords;
+    public TextMeshProUGUI SurvivedTime;
+    public Button Resume;
+    public Button Quit;
+
+    public bool Dead = false;
+
+
+    //"Date: Unknown  |  Year: 1900\nSurvived: "+ (int)time/60 +"Minutes and "+ (int)time + " Seconds";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button ResumeButton = Resume.GetComponent<Button>();
+        ResumeButton.onClick.AddListener(TryAgain);
+        Button QuitButton = Quit.GetComponent<Button>();
+        QuitButton.onClick.AddListener(MainMenu);
+    }
+
+    void EndGame()
+    {
+        Dead = true;
+        StartCoroutine(FadeTextIn(5f, EndWords));
+    }
+
+    IEnumerator FadeTextIn(float t, TextMeshProUGUI i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+        while (i.color.a < 1.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+            yield return null;
+        }
+        int time = (int)(gameObject.GetComponent<Pause>().getTime() - t);
+        SurvivedTime.text = "Survived: " + time / 60 + " Minutes and " + time + " Seconds";
+        Resume.gameObject.SetActive(true);
+        Quit.gameObject.SetActive(true);
+        SurvivedTime.gameObject.SetActive(true);
+    }
+
+    IEnumerator LoadingScene(string scene)
+    {
+        yield return new WaitForEndOfFrame();
+        SceneManager.LoadScene(scene);
+    }
+
+    void TryAgain()
+    {
+        Time.timeScale = 1;
+        StartCoroutine(LoadingScene("MainScene"));
+    }
+
+    void MainMenu()
+    {
+        Time.timeScale = 1;
+        StartCoroutine(LoadingScene("TitleScene"));
+    }
+}
