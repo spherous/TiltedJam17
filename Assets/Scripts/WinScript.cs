4a9d1acbb39de06e8bfbf02ac770c0173044bd94
Nettsuka <jonathanchhou@outlook.com>
2019-10-26 20:52:46 -0700

    Win canvas done

---
+++ Assets/Scripts/WinScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WinScript : MonoBehaviour
+{
+    public Button Home;
+    public Canvas WinCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button HomeButton = Home.GetComponent<Button>();
+        HomeButton.onClick.AddListener(GoHome);
+    }
+
+    IEnumerator LoadingScene(string scene)
+    {
+        yield return new WaitForEndOfFrame();
+        SceneManager.LoadScene(scene);
+    }
+
+    void Win()
+    {
+        WinCanvas.gameObject.SetActive(true);
+        gameObject.GetComponent<GameOver>().Dead = true;
+    }
+
+    void GoHome()
+    {
+        StartCoroutine(LoadingScene("TitleScene"));
+    }
+}
