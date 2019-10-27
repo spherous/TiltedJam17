b10505cce99ff485b992fc1d3202245f96f47855
Avea <aveakrause@gmail.com>
2019-10-26 17:18:20 -0700

    Added story

---
+++ Assets/Ink/Graveyard- Prologue.ink
@@ -0,0 +1,28 @@
+//start_prologue
+
+<em> la-da-dee-daaa-da-da-deeee</em>
+
+Your eyes slowly open to a child humming a haunting melody. 
+
+<em>"Uh, you woke up, finally. I thought you were going to stay here with us forever."</em>
+
+-> Prologue
+
+== Prologue ==
+
+    *You scan the area... 
+    
+    <em>"Well, don't just sit there silly, follow me. We only have a few hours before sunsrise and I'm running low on fuel."</em>
+    
+    **Who are you?
+    
+    <em>"I'm a gypsy queen, ballarina dancer, lover of life - but you can call me Little Miss Nina. I'll be your personal guide through our little playdate this fine evening." 
+    
+    ***What is this place?
+    
+    "Sweetie, there will be plenty of time for questions later. But right now, our first stop is to get me fuel and a word to the wise, stay close to the light. Ghouls are everywhere and you don't want to end up cursed like me. "</em>
+    
+    ->DONE
+    
+
+//end_prologue
---
+++ Assets/Scripts/PlayerChoiceText.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class PlayerChoiceText : MonoBehaviour, IPointerDownHandler
+{
+    public InkScript ink;
+
+    public TextMeshProUGUI playerChoiceText;
+
+    public Color32 orangeColor;
+
+    public float flashDuration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FlashWhite());
+    }
+
+    IEnumerator FlashWhite()
+    {
+        float t = 0;
+        bool running = true;
+        while(running)
+        {
+            if(playerChoiceText.color == Color.white)
+            {
+                StartCoroutine(FlashOrange());
+                running = false;
+            }
+            playerChoiceText.color = Color.Lerp(orangeColor, Color.white, t/flashDuration);
+            
+            t += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    IEnumerator FlashOrange()
+    {
+        float t = 0;
+        bool running = true;
+        while(running)
+        {
+            if(playerChoiceText.color == orangeColor)
+            {
+                StartCoroutine(FlashWhite());
+                running = false;
+            }
+            playerChoiceText.color = Color.Lerp(Color.white, orangeColor, t/flashDuration);
+            
+            t += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        ink.Next();
+    }
+}
